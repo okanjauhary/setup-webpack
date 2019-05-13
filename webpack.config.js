00deee85b21cbbce4cb8b0c180b5c2098bbaedcc
jauhary <ms.jauhary@gmail.com>
2019-05-13 23:22:44 +0700

    add rules of style loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: __dirname + "/src/app/index.js", // webpack entry point. Module to start building dependency graph
+  entry: __dirname + "/src/app/index.js",
   output: {
-    path: __dirname + '/dist', // Folder to store generated bundle
-    filename: 'bundle.js',  // Name of generated bundle after build
-    publicPath: '/' // public URL of the output directory when referenced in a browser
+    path: __dirname + '/dist',
+    filename: 'bundle.js', 
+    publicPath: '/'
   },
-  module: {  // where we defined file patterns and their loaders
+  module: { 
       rules: [
         {
             test: /\.js$/,
@@ -15,17 +15,27 @@ module.exports = {
             exclude: [
                 /node_modules/
             ]
+        },
+        {
+          test: /\.(sass|scss)$/,
+          use: [{
+              loader: "style-loader"
+          }, {
+              loader: "css-loader"
+          }, {
+              loader: "sass-loader"
+          }]
         }
       ]
   },
-  plugins: [  // Array of plugins to apply to build chunk
+  plugins: [ 
       new HtmlWebpackPlugin({
           template: __dirname + "/src/public/index.html",
           inject: 'body'
       })
   ],
-  devServer: {  // configuration for webpack-dev-server
-      contentBase: '/src/public',  //source of static assets
-      port: 5000, // port to run dev-server
+  devServer: { 
+      contentBase: '/src/public', 
+      port: 5000,
   } 
 }
