8405f1eaba998b1902bea6e17c9852e35e86d8e5
Sulhan Jauhary <ms.jauhary@gmail.com>
2019-10-12 18:04:16 +0700

    moving directories

--- src/app/index.js
+++
@@ -1,6 +0,0 @@
-import myclass from './myclass'
-import '../style/app.scss'
-
-let test = new myclass()
-
-console.log(test.msg)
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import myclass from './app/myclass'
+import './assets/scss/app.scss'
+
+let test = new myclass()
+
+console.log(test.msg)
