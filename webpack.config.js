f70b7fbcbd3f78335e937a1a53a95ecde5931f72
Sulhan Jauhary <ms.jauhary@gmail.com>
2019-10-12 18:03:55 +0700

    change configuration path of entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: __dirname + "/src/app/index.js",
+  entry: __dirname + "/src/index.js",
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js', 
